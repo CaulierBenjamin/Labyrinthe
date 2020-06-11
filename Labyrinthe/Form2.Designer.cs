823346ba3ad38dab1581ce3382ecb920ae8051f4
benjamin <benjamincaulier@live.fr>
2020-06-11 10:04:48 +0200

    11 juin , 10h pour cours v2

---
+++ Labyrinthe/Form2.Designer.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace Labyrinthe
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size((Map.publicPlateau.GetLength(0)+3)*30,(Map.publicPlateau.GetLength(1)+2)*30);
+            this.Name = "Form2";
+            this.Text = "Plateau";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picturebox1;
+    }
+}
